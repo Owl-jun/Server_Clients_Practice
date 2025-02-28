2d83e861aee4a06ed5a7deac42490d133647dda1
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-02-28 14:01:04 +0900

    today

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ import requests
 response = requests.post("http://127.0.0.1:5000/add", json={"content": "Flask 공부하기"})
 
 print(response.json())  # 응답 출력
+
+res = requests.get("http://127.0.0.1:5000/memos")
+print(res.json())
